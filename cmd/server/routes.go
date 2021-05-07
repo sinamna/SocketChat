9830e32cb5421adb87c7e3d6f13317db32b7daf1
sinamna <sina3050@gmail.com>
2021-05-07 20:04:44 +0430

    added mux

--- cmd/server/routes.go
+++ cmd/server/routes.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"github.com/bmizerany/pat"
+	"github.com/sinamna/SocketChat/internal/handlers"
+	"net/http"
+)
+
+func routes() http.Handler {
+	mux := pat.New()
+
+	mux.Get("/", http.HandlerFunc(handlers.Home))
+
+	return mux
+}
