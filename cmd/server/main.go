1a9ba5c9c7f3907811c472e93e6ad81dd9218f80
sinamna <sina3050@gmail.com>
2021-05-09 22:01:04 +0430

    added channel listener go routine

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"github.com/sinamna/SocketChat/internal/handlers"
 	"log"
 	"net/http"
 )
 
 func main(){
 	mux := routes()
+
+	log.Println("starting channel listener")
+	go handlers.ListenToPayloadChan()
+
 	log.Println("starting server on port 8080")
 	_ = http.ListenAndServe(":8080", mux)
 }
