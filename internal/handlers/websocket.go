09ff0e783afd5b68c804f924f108b49ea99ed411
sinamna <sina3050@gmail.com>
2021-05-09 21:42:05 +0430

    done loadpayload func

--- internal/handlers/websocket.go
+++ internal/handlers/websocket.go
@@ -6,43 +6,69 @@ import (
 	"net/http"
 )
 
-type WebSocketConn struct{
+type WebSocketConn struct {
 	*websocket.Conn
 }
 
-type WsJsonResponse struct{
-	Action string `json:"action"`
-	Message string `json:"message"`
+type WsJsonResponse struct {
+	Action      string `json:"action"`
+	Message     string `json:"message"`
 	MessageType string `json:"message_type"`
 }
 
-type WsPayload struct{
-	Username string `json:"username"`
-	Action string `json:"action"`
-	Message string `json:"message"`
-	Conn WebSocketConn `json:"-"`
+type WsPayload struct {
+	Username string        `json:"username"`
+	Action   string        `json:"action"`
+	Message  string        `json:"message"`
+	Conn     WebSocketConn `json:"-"`
 }
 
+var payloadChan = make(chan WsPayload)
+var clients = make(map[WebSocketConn]bool)
+
 var upgrader = websocket.Upgrader{
-	ReadBufferSize: 1024,
+	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
-	CheckOrigin: func (r *http.Request) bool { return true},
+	CheckOrigin:     func(r *http.Request) bool { return true },
 }
 
 func WsEndpoint(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 
-	if err!=nil{
+	if err != nil {
 		log.Println(err)
 	}
 
 	var res WsJsonResponse
 	res.Message = "connection successfully upgraded"
 
+	wsConn := WebSocketConn{Conn: conn}
+	clients[wsConn] = true
+
 	err = conn.WriteJSON(res)
 
-	if err!=nil{
+	if err != nil {
 		log.Println(err)
 	}
+	go LoadPayload(&wsConn)
+}
 
+func LoadPayload(conn *WebSocketConn) {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println(r)
+		}
+	}()
+
+	var payload WsPayload
+
+	for {
+		err := conn.ReadJSON(&payload)
+		if err != nil {
+			//no payload
+		} else {
+			payload.Conn =*conn
+			payloadChan <- payload
+		}
+	}
 }
